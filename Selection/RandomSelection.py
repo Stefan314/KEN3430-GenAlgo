d60bcd721c5e0c8d868046b70ec343dacd46d1c6
Mc1cheeseburger <43991581+Mc1cheeseburger@users.noreply.github.com>
2022-03-15 11:28:36 +0100

    small update to randomselection

--- Selection/RandomSelection.py
+++ Selection/RandomSelection.py
@@ -1,5 +1,6 @@
 from Individual import Individual
 from Selection.SelectionType import SelectionType
+import random
 
 name = "Random"
 
@@ -10,4 +11,12 @@ class RandomSelection(SelectionType):
 
     def select(self, pop: list[Individual]) -> Individual:
         # TODO: Implement
+        # we take random Individuals, for a total of half the population. Repetitions are allowed.
+        n = pop.len/2
+        random.shuffle(pop)
+        new_pop = list[Individual]
+        for i in n:
+            random_selection = random.randrange(len(pop))
+            new_pop.append(pop[random_selection])
+
         pass
